b289a79cf60c86b76c30ebc1a83a5e590c00898f
aki <44888139+aki-tera@users.noreply.github.com>
2019-12-29 13:25:10 +0900

    Ver1.2:click_by_xpath.js

--- click_by_xpath.js
+++ click_by_xpath.js
@@ -4,5 +4,10 @@ javascript:(function(){
 	n=document.getElementById("userpass");
 	n.value="your_pass";
 	n=document.evaluate("xpath_of_click", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+	
+	example)
+	 //*[@id="loginbox"]/div[2]/input     selected by full path
+	 //input[@type="image"]               selected by <input>'s element and type="image"'s attribute
+	
 	n.snapshotItem(0).click();
 })();
