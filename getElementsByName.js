635cd08607f329dd5a8096b6811c486ea8aaa9ef
aki <44888139+aki-tera@users.noreply.github.com>
2019-12-29 13:26:51 +0900

    Ver1.3:getElementsByName.js

--- getElementsByName.js
+++ getElementsByName.js
@@ -3,5 +3,6 @@ javascript:(function(){
   n.item(0).value="your_name";
   n=document.getElementsByName("userpass");
   n.item(0).value="your_pass";
+  n=document.getElementById("userlogin");
   n.item(0).submit();
 })();
