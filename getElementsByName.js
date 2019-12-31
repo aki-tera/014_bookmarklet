e23260cfda0b40d52002bec05feace775165bcde
aki <44888139+aki-tera@users.noreply.github.com>
2019-12-31 11:38:13 +0900

    Ver1.6:amend getElementsByName.js

--- getElementsByName.js
+++ getElementsByName.js
@@ -1,8 +1,8 @@
 javascript:(function(){
   n=document.getElementsByName("username");
-  n.item(0).value="your_name";
+  n[0].value="your_name";
   n=document.getElementsByName("userpass");
-  n.item(0).value="your_pass";
+  n[0].value="your_pass";
   n=document.getElementById("userlogin");
-  n.item(0).submit();
+  n[0].submit();
 })();
