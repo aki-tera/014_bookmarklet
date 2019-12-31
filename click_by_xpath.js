b1141f5df20f018dba5057658f88cb79911dcd8a
aki <44888139+aki-tera@users.noreply.github.com>
2019-12-31 11:33:59 +0900

    Ver1.5:amend click_by_xpath.js

--- click_by_xpath.js
+++ click_by_xpath.js
@@ -1,8 +1,8 @@
 javascript:(function(){
-	n=document.getElementById("username");
-	n.value="your_name";
-	n=document.getElementById("userpass");
-	n.value="your_pass";
+	n=document.evaluate("xpath_username", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+	n.snapshotItem(0).value="your_name"
+	n=document.evaluate("xpath_userpass", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+	n.snapshotItem(0).value="your_pass"
 	n=document.evaluate("xpath_of_click", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 	
 	example)
